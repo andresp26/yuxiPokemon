492768e7958ea70f09f40d189f16615976232076
andres <andresp-26@hotmail.com>
2020-09-12 23:31:55 -0500

    Jasmin Unit  Test

---
+++ src/app/pages/login/login.component.spec.ts
@@ -0,0 +1,42 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule,
+            ReactiveFormsModule,
+            FormsModule 
+          ],
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }); 
+
+ 
+ 
+  it('should test form validity', () => {
+    const form = component.frmLogin;
+    expect(form.valid).toBeFalsy();
+
+    const nameInput = form.controls.email;
+    nameInput.setValue('acpleno93@gmail.com');
+
+    expect(form.valid).toBeTruthy();
+  })
+
+  
+});
